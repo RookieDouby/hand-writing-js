59dd0e89b77f2199c0d97dee5a814a187b19532d
张红豆 <zhanghongdou@enn.cn>
2020-11-17 01:00:04 +0800

    简单的promise

--- promise/myPromise.js
+++ promise/myPromise.js
@@ -3,13 +3,13 @@
  * @description 手写 Promise
  */
 
-const PENDIND = 'pending';
+const PENDING = 'pending';
 const RESOLVED = 'resolved';
 const REJECTED = 'rejected';
 
-function MyPromise(fn) {
+function myPromise(fn) {
   const that = this;
-  that.status = PENDIND;
+  that.status = PENDING;
   that.value = null;
   that.reason = null;
 
@@ -17,12 +17,55 @@ function MyPromise(fn) {
   that.rejectedCallbacks = [];
 
   function resolve(value) {
-    // if (that.status =)
+    if (that.status === PENDING) {
+      that.status = RESOLVED;
+      that.value = value;
+      that.resolvedCallbacks.map(cb => cb(value));
+    }
+  }
+
+  function reject(reason) {
+    if (that.status === PENDING) {
+      that.status = REJECTED;
+      that.reason = reason;
+      that.rejectedCallbacks.map(cb => cb(reason));
+    }
   }
 
   try {
-    fn(resolve, reject)
+    fn(resolve, reject);
   } catch(e) {
     reject(e);
   }
 }
+
+myPromise.prototype.then = function(onFullfilled, onRejected) {
+  const that = this;
+  
+  if (that.status === PENDING) {
+    that.resolvedCallbacks.push(onFullfilled);
+    that.rejectedCallbacks.push(onRejected);
+  }
+
+  if (that.status === RESOLVED) {
+    onFullfilled(that.value);
+  }
+
+  if (that.status === REJECTED) {
+    onFullfilled(that.reason);
+  }
+
+  return that;
+}
+
+const p = new myPromise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(1000);
+  }, 1000);
+});
+
+p.then((res) => {
+  console.log('结果：', res); // 结果：1000
+}).then(() => {
+  console.log('jsliang'); // jsliang
+})
